75243e0506ca3f31f8d1e65f6ef9b39c5c8028ad
Duke Woy <taolimon@disroot.org>
2024-03-21 17:19:57 +0000

    started creating the rpi interface

--- Beta/user_interface/rpi-interface.py
+++ Beta/user_interface/rpi-interface.py
@@ -1,13 +1,19 @@
 import sys
 import time
 import csv
-import matplotlib as mlp
+import matplotlib as mpl
+import matplotlib.animation as animation
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QWidget, QLabel, QToolBar
 )
 
 class MainWindow(QMainWindow):
+
+    # Graph variables
+    fig = mpl.figure()
+    ax1 = fig.add_subplot(1,1,1)
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -24,6 +30,24 @@ class MainWindow(QMainWindow):
 
     def onToolBarButtonClick(self, s):
         print("clicked", s)
+
+    def animate(self, i):
+        graph_data = open('graph/graph_data.csv', 'r').read()
+        lines = graph_data.split('\n')
+        x_axis = []
+        y_axis = []
+        for line in lines:
+            if len(line) > 1:
+                measured_time, duration = line.split(',')
+                x_axis.append(float(measured_time))
+                y_axis.appen(float(duration))
+        self.ax1.clear()
+        self.ax1.plot(x_axis, y_axis)
+        return
+    
+    # Animation variables
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    mpl.show()
 
 
 # Every qt program must have an app
